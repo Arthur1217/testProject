dd0cae8e840f0ec1ad4b0c6192f10f9e9edf08fe
Arthur1217 <arthurzhang1217@gmail.com>
2017-12-08 11:37:17 +0800

    【增加测试目录】

---
+++ testModule/src/main/java/com/arthur/test/Test.java
@@ -0,0 +1,11 @@
+package com.arthur.test;
+
+/**
+ * @author ArthurZhang
+ * @version 2017/12/8 11:20
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
